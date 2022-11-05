a2ad0150c6616fa8ac0cf1e02bb97d7fb94bf180
deocksoo <deocks@deering.co>
2022-11-05 19:32:40 +0900

    build.gradle.kts 정리 & 불필요한 테스트 파일 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm") version "1.6.21"
 }
 
 group = "com.deer"
@@ -12,13 +11,26 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.3")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.3")
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
-tasks.test {
+val kotestVersion = "5.5.2"
+
+apply(plugin = "org.jetbrains.kotlin.jvm")
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
@@ -27,8 +39,4 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
-}
-
-application {
-    mainClass.set("MainKt")
 }
